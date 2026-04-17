e66aecdbb84e9578aeb012de48e801b68ca2921b
Anirudh Jena <jenaanirudh05@gmail.com>
2026-04-17 21:59:37 +0530

    learning JSX

---
+++ src/components/greet.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+
+// function greet(){
+//     return <h1>Hello Anirudh</h1>
+// }
+
+const greet = () => {
+    return <h1>Hello Anirudh</h1>
+}
+
+export default greet; 
+
