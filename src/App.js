17d25074b8c2b8fd1b447f20813c6b34352dedfb
Anirudh Jena <jenaanirudh05@gmail.com>
2026-04-18 09:48:28 +0530

    learning state and setState

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import './App.css';
 import Greet from './components/greet';
 import Welcome from './components/welcome';
 import Hello from './Learning JSX';
+import counter from './components/counter';
+import Counter from './components/counter';
 
 function App() {
   return (
-    <div className="App">
-      <Greet name="Anirudh">
-        <p>He is a Good boy</p>
-      </Greet>
-      <Greet name="Yash"/>
-      <Greet name="Ashutosh"/>
-      <Greet name="Siddhart"/>
-
-      <Welcome name ="Anirudh"/>
-    </div>
+    <Counter/>
   );
 }
 
